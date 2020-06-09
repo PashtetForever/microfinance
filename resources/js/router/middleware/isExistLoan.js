6714b2193da05d1a0f29226e842f27188d17af63
PashtetForever <nik-kostukov@yandex.ru>
2020-06-09 22:30:39 +0300

    Исправлен посредник наличия займа

--- resources/js/router/middleware/isExistLoan.js
+++ resources/js/router/middleware/isExistLoan.js
@@ -1,6 +1,6 @@
 export default async function isExistLoan({next, store}){
   const response = await store.dispatch('isExistLoan');
-  if(!response.hasOwnProperty('data'))
+  if(!response.hasOwnProperty('data') && response.data !== false)
     return next({name: 'order-form'})
   return next();
 }
