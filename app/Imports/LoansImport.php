e2a9f7aa063c25b9e1103b16b9535875f9d5718a
PashtetForever <nik-kostukov@yandex.ru>
2020-06-10 17:02:44 +0300

    Сделан импорт займов

--- app/Imports/LoansImport.php
+++ app/Imports/LoansImport.php
@@ -7,17 +7,33 @@ use Maatwebsite\Excel\Concerns\ToModel;
 use App\Models\Loan;
 use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use App\Services\Exchange1C\API;
 
 class LoansImport implements ToModel, WithCustomCsvSettings, WithHeadingRow
 {
     use Importable;
 
+    private API $api;
+    private string $sessionId;
+
+    public function __construct()
+    {
+        $this->api = app()->get(API::class);
+        $data = (array)$this->api->login('test', 'test')->getData(true);
+        $this->sessionId = $data['SessionId'];
+    }
+
     public function model($row)
     {
+        $loanData = $this->api->getLastContractData($this->sessionId, $row['loan_guid']);
+
         return new Loan([
             'number' => $row['number'],
             'loan_guid' => $row['loan_guid'],
-            'user_guid' => $row['user_guid']
+            'user_guid' => $row['user_guid'],
+            'days' => $loanData['Days'],
+            'sum' => (int)str_replace(' ', '', $loanData['Sum']),
+            'percent' => (float)str_replace(',', '.',$loanData['Percent'])
         ]);
     }
 
