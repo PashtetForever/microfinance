3e4e388404fc51d0d5776484a4f07eed7e27d792
PashtetForever <nik-kostukov@yandex.ru>
2020-06-16 16:11:31 +0300

    Исправллена ошибка округления процентов в погашении займа

--- resources/js/components/Profile/LoanExtension.vue
+++ resources/js/components/Profile/LoanExtension.vue
@@ -163,7 +163,7 @@
         await this.$store.dispatch('loadContactData');
 
       this.contractData = await this.$store.dispatch('getValidContract');
-      this.percentSum = _.parseFloat(this.contractData.PercentSum) + _.parseFloat(this.contractData.Penalty);
+      this.percentSum = _.toNumber(this.contractData.PercentSum) + _.toNumber(this.contractData.Penalty);
       this.returnDate =  moment(this.contractData.ReturnDate, 'DD.MM.YYYY').locale('ru').add(1, 'days');
     },
     watch: {
