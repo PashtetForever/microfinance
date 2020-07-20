b2cba29d1036e3d9a3c680d6b651b9c94fa36369
PashtetForever <nik-kostukov@yandex.ru>
2020-07-20 11:37:02 +0300

    Добавлена команда добавления подписанного договара в займ

---
+++ app/Console/Commands/AddSignContract.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Loan;
+use App\Services\Exchange1C\API;
+use App\UseCases\Loan\DocumentsService;
+use App\UseCases\Loan\LoanService;
+use Illuminate\Console\Command;
+
+class AddSignContract extends Command
+{
+    protected $signature = 'loan:add-sign-contract {loanGuid}';
+    protected $description = 'Добавление подписанного договора к займу';
+
+    /**
+     * @var LoanService
+     */
+    private LoanService $loanService;
+
+    /**
+     * @var API
+     */
+    private API $api;
+
+    /**
+     * @var DocumentsService
+     */
+    private DocumentsService $documentsService;
+
+    public function __construct(LoanService $loanService, DocumentsService $documentsService, API $api)
+    {
+        parent::__construct();
+
+        $this->loanService = $loanService;
+        $this->api = $api;
+        $this->documentsService = $documentsService;
+    }
+
+    public function handle()
+    {
+        $loanGuid = $this->argument('loanGuid');
+
+        $loginData = (array)$this->api->login('test', 'test')->getData(true);
+        $loan = Loan::whereLoanGuid($loanGuid)->first();
+
+        if(!$loan) {
+            $this->error('Не найден зйм с GUID ' . $loanGuid);
+            return;
+        }
+
+        $signContractFile = $this->api->requestSignContract($loginData['SessionId'], '1', $loan->loan_guid);
+        $this->documentsService->addDocumentToLoan(
+            $loan, $loginData['SessionId'], $signContractFile['Description'], $signContractFile['FileName'], '1');
+
+        $this->info('Выполнено');
+    }
+}
