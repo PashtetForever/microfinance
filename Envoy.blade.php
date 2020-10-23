d21df38083df9e89171a0bd8535717f65ea34f7c
PashtetForever <nik-kostukov@yandex.ru>
2020-10-23 13:03:26 +0300

    Обновили node модули

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -19,7 +19,7 @@
 @task('test')
     cd /var/www/zaim-dynai/data/www/zaim-dynai.ru/test/
     echo "Pulling repo..."
-    git pull --force origin master
+    git pull origin master
 
     echo "Run migrations..."
     /opt/php74/bin/php artisan migrate --force --no-interaction
